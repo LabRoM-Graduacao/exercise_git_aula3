7f6df13474a793a3b3d125ea942ff8f4a02f411c
JPBG-USP <joao_garciajp@usp.br>
2026-02-24 14:09:46 -0300

    Adding line break in dividindo,py

--- scripts/dividindo.py
+++ scripts/dividindo.py
@@ -3,4 +3,5 @@ num2 = float(input("Insira outro número: "))
 
 result = num1 / num2
 
-print(f"O resultado de {num1}/{num2} = {result}")
+print(f"O resultado da divisão:
+    {num1}/{num2} = {result}")
